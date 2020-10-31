03fd4ff7bc536dccc6ec3fced1c563c0909942f1
WillV1 <wmvanhook@gmail.com>
2020-10-31 09:45:53 -0400

    Update settings to remove secret

---
+++ wayfarer_project/secret.py
@@ -0,0 +1,2 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '_^(p)rckb@n$#2iu#)8erfa8pq)ks*e*i@zfa(&xzd4mr*@uei'
--- wayfarer_project/settings.py
+++ wayfarer_project/settings.py
@@ -1,3 +1,5 @@
+from .secret import *
+
 """
 Django settings for projectwayfarer_project project.
 
@@ -19,8 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_^(p)rckb@n$#2iu#)8erfa8pq)ks*e*i@zfa(&xzd4mr*@uei'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

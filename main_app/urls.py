b04a0107072e2f2009881d6de905fd3ca17508a2
WillV1 <wmvanhook@gmail.com>
2020-11-04 12:48:41 -0500

    Add post function

--- main_app/urls.py
+++ main_app/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('profile/new/', views.new_profile, name='new_profile'),
     path('profile/<int:profile_id>/', views.user_profile, name='user_profile'),
     path('profile/', views.profile, name='profile'),
+    # path('city/<int:city_id>/add_post/', views.add_post, name='add_post'),
     path('city/<int:city_id>/add_post/', views.add_post, name='add_post'),
     path('post/<int:post_id>/edit/', views.edit_post, name='edit_post'),
     path('city/<int:city_id>/remove_post/<int:post_id>/', views.delete_post, name='delete_post'),

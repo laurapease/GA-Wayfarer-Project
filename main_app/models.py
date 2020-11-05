6fff4f636a332e5958368917663950700fd51870
WillV1 <wmvanhook@gmail.com>
2020-11-05 10:27:52 -0500

    Add truncate characters

--- main_app/models.py
+++ main_app/models.py
@@ -1,16 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
 
 # Create your models here.
 
 class City(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='images')
+    slug = models.SlugField(max_length=100, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super(City, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
-
 
 class Profile(models.Model):
     current_city = models.CharField(max_length=100)
@@ -23,8 +30,8 @@ class Profile(models.Model):
         return self.user.username
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField(max_length=250)
+    title = models.CharField(max_length=200)
+    content = models.TextField(max_length=2000)
     timestamp = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     city = models.ForeignKey(City, on_delete=models.CASCADE)

7947ec1fc38adf761c77492d65f071b8ba959c7f
WillV1 <wmvanhook@gmail.com>
2020-11-05 20:08:09 -0500

    Remove pycashes

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,13 +8,10 @@ from django.conf.urls import include, url
 
 urlpatterns = [
     path('', views.home, name='home'),
-    # path('about/', views.about, name='about'),
     path('accounts/signup/', views.signup, name='signup'),
     path('profile/new/', views.new_profile, name='new_profile'),
     path('profile/<int:profile_id>/', views.user_profile, name='user_profile'),
     path('profile/', views.profile, name='profile'),
-    # path('profile/', views.profile, name='profile'),
-    # path('city/<int:city_id>/add_post/', views.add_post, name='add_post'),
     path('city/<int:city_id>/add_post/', views.add_post, name='add_post'),
     path('post/<int:post_id>/edit/', views.edit_post, name='edit_post'),
     path('city/<int:city_id>/remove_post/<int:post_id>/', views.delete_post, name='delete_post'),
